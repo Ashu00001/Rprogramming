36f0bb8a6b60f516a81e3ab0952b2c41bc62c807
Ashutosh Gupta <43481150+Ashu00001@users.noreply.github.com>
2020-09-14 10:52:08 +0530

    Create array as vector.r

---
+++ array as vector.r
@@ -0,0 +1,6 @@
+a=as.integer(readline(prompt="enter starting point "))
+b=as.integer(readline(prompt="enter end point "))
+                           
+ vector1<-a:b
+ vector1<-as.array(vector1)
+ print(vector1)
